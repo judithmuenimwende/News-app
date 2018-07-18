9ca80f2c452722efa68f36bcbf73bd9c861326c8
Judith Mueni <judithmueni395@gmail.com>
2018-07-18 12:29:49 +0300

    modified changes

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,78 @@
 <template>
   <div class="container">
-    <h3>{{ title }}</h3>
-    <form>
-      <textarea placeholder="Add new task" v-model="newTask"></textarea>
-      <p>{{newTask}}</p>
 
-      <button type="button" v-on:click="addTask">Add Task {{tasks.length}}</button>
+    <h1>Trending news</h1>
 
-    </form>
-    <ul>
-      <li v-for="task in tasks">{{task}}</li>
-    </ul>
+    <input v-model="search" type="text" name="search" placeholder="Search..">
+    <button v-on:click="loadNews" type="button">Search</button>
+
+    <p v-if="loading">Please wait...</p>
+    
+    <div v-for="article in news">
+      
+      
+      <img v-if="article.urlToImage" :src="article.urlToImage" alt="">
+
+      <h3>{{article.title}} </h3>
+
+      <h4>{{article.author}}</h4>
+
+      <a class="button" :href="article.url" target="_blank">Read more</a>
+
+    </div>
+
   </div>
 </template>
 
+
 <script>
+
+import axios from 'axios'
+
+const baseUrl = "https://newsapi.org/v2";
+const apikey = "8398f125526a43beaaf82b35de2ef3c6";
+const endpoint = "/everything";
+
+const data = {
+  news: [],
+  search:'',
+  loading:false
+}
+
+
 
 export default {
   data: function() {
-    return {
-      title: 'My Task Manager',
-      newTask: 'This is a SAMPLE ',
-      tasks: []
-    }
+    return data 
   },
+
+  created() {
+    this.loadNews();
+  },
+
   methods: {
-    addTask() {
-      this.tasks.unshift(this.newTask);
-      this.newTask = '';
+      loadNews() {
+        if(this.search.length < 1) {
+          return;
+        }
+
+        this.loading=true;
+
+        this.news = [];
+
+      let url = baseUrl + endpoint + '?q=' + this.search + '&apikey=' + apikey;
+
+
+
+      axios.get(url).then(function(response) {
+        console.log(response.data.articles)
+        data.loading=false;
+        data.news = response.data.articles
+        
+        
+      }).catch(function(error) {
+          console.log(error.message)
+      })
     }
   }
 }
